eafbdb5d25ac4cf3f99e77b4a0c8f892275fdff9
Cathal Minnock <minnockc@hpc01.tchpc.tcd.ie>
2019-08-28 13:33:38 +0100

    Changed autocorrelation.py to write to file

--- autocorrelation.py
+++ autocorrelation.py
@@ -1,11 +1,11 @@
 import numpy as np
 import sys
 
-filename = sys.argv[1]
+input_filename = sys.argv[1]
 output_filename = sys.argv[2]
 tau = int(sys.argv[3])
 
-data = np.loadtxt(filename, unpack = True)
+data = np.loadtxt(input_filename, unpack = True)
 N = len(data)
 mean = np.mean(data)
 
@@ -17,4 +17,7 @@ for i in range(N-tau):
 for i in range(N-tau, N):
 		bottom += (data[i] - mean)**2
 autocorrelation = top / bottom
-print(autocorrelation)
+
+f=open(output_filename, "a+")
+f.write(str(tau) + " " str(autocorrelation))
+f.close() 
