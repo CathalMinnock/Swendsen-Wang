8592530caf20bf78a4ab13a6940352c7f54b67ed
Cathal Minnock <minnockc@hpc01.tchpc.tcd.ie>
2019-09-02 16:04:10 +0100

    small change in plotting.py

--- plotting.py
+++ plotting.py
@@ -6,24 +6,26 @@ q=3
 size=16
 directory_name="SW_q_" + str(q) + "_size_" + str(size) + "_n_" + str(n) + "_therm_0"
 
+x = np.arange(1000)
+
 beta = ".2000"
 filename=directory_name + "/" + directory_name + "_beta_" + beta + ".txt"
-x, y  = np.loadtxt(filename, unpack = True)
+y  = np.loadtxt(filename, unpack = True)
 plt.plot(x, y, 'o', label = "beta = 0.2")
 
 beta = ".3000"
 filename=directory_name + "/" + directory_name + "_beta_" + beta + ".txt"
-x, y  = np.loadtxt(filename, unpack = True)
+y  = np.loadtxt(filename, unpack = True)
 plt.plot(x, y, 'o', label = "beta = 0.3")
 
 beta = ".4000"
 filename=directory_name + "/" + directory_name + "_beta_" + beta + ".txt"
-x, y  = np.loadtxt(filename, unpack = True)
+y  = np.loadtxt(filename, unpack = True)
 plt.plot(x, y, 'o', label = "beta = 0.4")
 
 beta = ".5000"
 filename=directory_name + "/" + directory_name + "_beta_" + beta + ".txt"
-x, y  = np.loadtxt(filename, unpack = True)
+y  = np.loadtxt(filename, unpack = True)
 plt.plot(x, y, 'o', label = "beta = 0.5")
 
 plt.xlabel("Number of Swendsen-Wang iterations")
