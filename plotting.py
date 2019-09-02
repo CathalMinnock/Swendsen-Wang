e2f92cc273a19a0a385c666b7660d13142608e9f
Cathal Minnock <minnockc@hpc01.tchpc.tcd.ie>
2019-09-02 16:00:34 +0100

    Changed plotting.py

--- plotting.py
+++ plotting.py
@@ -1,40 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-n=4
-q=2
-beta = ".5000"
-size=8
-directory_name="SW_q_" + str(q) + "_size_" + str(size) + "_n_" + str(n)
-
-filename=directory_name + "/" + directory_name + "_beta_" + beta + "_autocorrelation.txt"
-x, y  = np.loadtxt(filename, unpack = True)
-plt.plot(x, y, 'o', label = "beta = 0.1")
+n=8
+q=3
+size=16
+directory_name="SW_q_" + str(q) + "_size_" + str(size) + "_n_" + str(n) + "therm_0"
 
 beta = ".2000"
-filename=directory_name + "/" + directory_name + "_beta_" + beta + "_autocorrelation.txt"
+filename=directory_name + "/" + directory_name + "_beta_" + beta + ".txt"
 x, y  = np.loadtxt(filename, unpack = True)
 plt.plot(x, y, 'o', label = "beta = 0.2")
 
 beta = ".3000"
-filename=directory_name + "/" + directory_name + "_beta_" + beta + "_autocorrelation.txt"
+filename=directory_name + "/" + directory_name + "_beta_" + beta + ".txt"
 x, y  = np.loadtxt(filename, unpack = True)
 plt.plot(x, y, 'o', label = "beta = 0.3")
 
 beta = ".4000"
-filename=directory_name + "/" + directory_name + "_beta_" + beta + "_autocorrelation.txt"
+filename=directory_name + "/" + directory_name + "_beta_" + beta + ".txt"
 x, y  = np.loadtxt(filename, unpack = True)
 plt.plot(x, y, 'o', label = "beta = 0.4")
 
 beta = ".5000"
-filename=directory_name + "/" + directory_name + "_beta_" + beta + "_autocorrelation.txt"
+filename=directory_name + "/" + directory_name + "_beta_" + beta + ".txt"
 x, y  = np.loadtxt(filename, unpack = True)
 plt.plot(x, y, 'o', label = "beta = 0.5")
 
 plt.xlabel("Number of Swendsen-Wang iterations")
-plt.ylabel("Autocorrelation Function")
-plt.title("16^3 lattice, Q=2, various inverse temperatures (beta)")
+plt.ylabel("Magnetization")
+plt.title("16^3 lattice, Q=3, various inverse temperatures (beta)")
 plt.grid(True)
 plt.xlim(0,10)
-plt.legend(loc = "upper right")
+plt.legend(loc = "lower right")
 plt.show()
